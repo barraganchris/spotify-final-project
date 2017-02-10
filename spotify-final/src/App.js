4bcff60d7a9047a7d43ded4c90bfce58b9bece4f
christopher barragan <barraganchris95@gmail.com>
2017-02-09 19:11:55 -0600

    finally staged and commited the app, index

--- spotify-final/src/App.js
+++ spotify-final/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import { hashHistory } from 'react-router';
+// ABOVE is all imported from React
+
+import axios from 'axios';
+import Header from './components/Header';
+import Footer from './components/Footer'
+import Landing from './components/Landing';
+// ABOVE are imported Components
+
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        {/* <Nav /> */}
+          <div className="main-content">
+            { this.props.children || <Landing /> }
+          </div>
+        <Footer />
       </div>
     );
   }
--- spotify-final/src/index.js
+++ spotify-final/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Link, hashHistory } from 'react-router';
+
 import App from './App';
-import './index.css';
+import Landing from './components/Landing';
+import About from './components/About';
+import Home from './components/Home';
+import Search from './components/Search';
 
 ReactDOM.render(
-  <App />,
+  <Router history={ hashHistory }>
+      <Route path="/" component={ App }>
+        <IndexRoute component={ Landing } />
+        <Route path="/home" component={ Home }/>
+        <Route path="/search" component={ Search }/>
+        <Route path="/about" component={ About }/>
+      </Route>
+    </Router>,
   document.getElementById('root')
 );
